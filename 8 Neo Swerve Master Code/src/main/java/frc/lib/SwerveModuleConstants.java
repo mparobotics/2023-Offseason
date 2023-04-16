7acd58c21b3da1636897f8c16ee135dbb5222ccd
ifrankel24 <ifrankel24@moundsparkacademy.org>
2023-04-16 11:25:29 -0500

    i think i fixed the gi(t)ssue

--- 8 Neo Swerve Master Code/src/main/java/frc/lib/SwerveModuleConstants.java
+++ 8 Neo Swerve Master Code/src/main/java/frc/lib/SwerveModuleConstants.java
@@ -7,7 +7,10 @@ public class SwerveModuleConstants {
   public final int angleMotorID;
   public final int cancoderID;
   public final Rotation2d angleOffset;
-  publ
+  public final double angleKP; 
+  public final double angleKI;
+  public final double angleKD;
+  public final double angleKFF;
 
   /**
    * Swerve Module Constants to be used when creating swerve modules.
@@ -18,10 +21,14 @@ public class SwerveModuleConstants {
    * @param angleOffset
    */
   public SwerveModuleConstants(
-      int driveMotorID, int angleMotorID, int canCoderID, Rotation2d angleOffset) {
+      int driveMotorID, int angleMotorID, int canCoderID, Rotation2d angleOffset, double angleKP, double angleKI, double angleKD, double angleKFF) {
     this.driveMotorID = driveMotorID;
     this.angleMotorID = angleMotorID;
     this.cancoderID = canCoderID;
     this.angleOffset = angleOffset;
+    this.angleKP = angleKP;
+    this.angleKI = angleKI;
+    this.angleKD = angleKD;
+    this.angleKFF = angleKFF;
   }
 }
